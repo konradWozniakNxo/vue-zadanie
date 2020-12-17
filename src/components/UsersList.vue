4dcd7b083a117aabbde2ef7a9fc98930c9a82611
konradWozniakNxo <73100068+konradWozniakNxo@users.noreply.github.com>
2020-12-17 10:46:46 +0100

    develop/ dodanie parametru do okienka V-dialog

--- src/components/UsersList.vue
+++ src/components/UsersList.vue
@@ -36,56 +36,56 @@
             <tbody>
               <tr v-for="item in list" v-bind:key="item.id">
                 <td>{{item.id}}</td>
-                <td><router-link :to="{ name: 'user', params: { id:item.id }}">{{item.name}}</router-link></td>
+                <td><router-link :to="{name: 'user', params: { id:item.id }}">{{item.name}}</router-link></td>
                 <td>{{item.username}}</td>
                 <td>{{item.email}}</td>
                 <td>{{item.phone}}</td>
                 <td>{{item.company.name}}</td>
-                <td><v-btn icon @click="dialog = true"><v-icon color="darken-2">mdi-arrow-left</v-icon></v-btn></td>                 
-                  <template>
-                    <v-row justify="center">
-                      <v-dialog 
-                        persistent
-                        v-model="dialog"
-                        width="600px"
-                      >                 
-                        <v-card>
-                          <v-card-title>
-                            <span class="headline">Czy na pewno usunać użytkownika</span>
-                          </v-card-title>
-                          <v-card-text>
-                          {{item.name}}
-                          </v-card-text>      
-                          <v-alert
-                            dense
-                            border="left"
-                            type="warning"
-                          >
-                            Zostaniesz wylogowany i utracisz dostęp do systemu
-                          </v-alert>
-                          <v-card-actions>
-                            <v-spacer></v-spacer>
-                            <v-btn 
-                              color="grey darken-1"
-                              text
-                              @click="dialog = false"
-                            >
-                              Anuluj
-                            </v-btn>
-                            <v-btn
-                              color="orange darken-1"
-                              text
-                              @click="dialog = false"
-                            >
-                              Usuń
-                            </v-btn>
-                          </v-card-actions>
-                        </v-card>
-                      </v-dialog>
-                    </v-row>
-                  </template>
+                <td><v-btn icon @click="dialogName(item.name)"><v-icon color="darken-2">mdi-arrow-left</v-icon></v-btn></td>                 
               </tr> 
             </tbody>
+             <template>
+              <v-row justify="center">
+                <v-dialog 
+                  persistent
+                  v-model="dialog"
+                  width="600px"
+                >                 
+                  <v-card>
+                    <v-card-title>
+                      <span class="headline">Czy na pewno usunać użytkownika</span>
+                    </v-card-title>
+                    <v-card-text>
+                    {{userName}}
+                    </v-card-text>      
+                    <v-alert
+                      dense
+                      border="left"
+                      type="warning"
+                    >
+                      Zostaniesz wylogowany i utracisz dostęp do systemu
+                    </v-alert>
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn 
+                        color="grey darken-1"
+                        text
+                        @click="dialog = false"
+                      >
+                        Anuluj
+                      </v-btn>
+                      <v-btn
+                        color="orange darken-1"
+                        text
+                        @click="dialog = false"
+                      >
+                        Usuń
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+              </v-row>
+            </template>
           </template>
         </v-simple-table>
       </v-list-item-content>
@@ -111,6 +111,13 @@ export default {
     this.$store.dispatch("getList", {
     });   
   },
+
+  methods:{
+         dialogName (name){
+              this.dialog=true;
+              this.userName =name;
+           }
+         }
 }
 </script>
 
